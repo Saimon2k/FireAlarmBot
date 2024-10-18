7741271c04e532dcafb23aecc7f3cf30a12532f9
absalyamov <absalyamov@saut.ru>
2024-10-18 12:17:44 +0500

    Дополнил /joke

---
+++ FireAlarmBot/FloorService.cs
@@ -0,0 +1,56 @@
+using System.Collections.Concurrent;
+
+public class FloorService
+{
+    private readonly List<int> _floorLogs = new();
+    private readonly Timer _timer;
+
+    public FloorService()
+    {
+        _timer = new Timer(ClearLogs, null, TimeSpan.FromHours(1), TimeSpan.FromHours(1));
+    }
+
+    public void AddFloor(int floor)
+    {
+        _floorLogs.Add(floor);
+    }
+
+    public IEnumerable<int> GetUniqueFloors()
+    {
+        return _floorLogs.Distinct().OrderBy(x => x);
+    }
+
+    private void ClearLogs(object state)
+    {
+        _floorLogs.Clear();
+    }
+
+    public int GetTotalFloorsChecked()
+    {
+        return _floorLogs.Count;  // Общее количество проверок этажей
+    }
+
+    public int GetUniqueFloorCount()
+    {
+        return _floorLogs.Distinct().Count();  // Количество уникальных этажей
+    }
+
+    public int GetMostCheckedFloor()
+    {
+        return _floorLogs.GroupBy(f => f)
+            .OrderByDescending(g => g.Count())
+            .Select(g => g.Key)
+            .FirstOrDefault();  // Самый часто проверяемый этаж
+    }
+
+    public int GetLastCheckedFloor()
+    {
+        return _floorLogs.LastOrDefault();  // Последний проверенный этаж
+    }
+
+    public double GetCheckedPercentage()
+    {
+        float uniqueFloors = GetUniqueFloorCount();
+        return uniqueFloors / 25f * 100f;  // Процент проверенных этажей
+    }
+}
